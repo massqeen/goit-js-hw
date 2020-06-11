4bce340deff41eb1f58eab3366986a805623deea
massqeen <62159695+massqeen@users.noreply.github.com>
2020-06-11 11:17:26 +0300

    cleared tasks for hw2

--- js/task-02.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-let ordered = 50;
-let total = 100;
-
-if (ordered >= total) {
-    console.log('На складе недостаточно товаров!');
-} else console.log('Заказ оформлен, с вами свяжется менеджер');
