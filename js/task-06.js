1c6eedcb91fa9e5c828d7949ca427579f72194a8
massqeen <massqeen@gmail.com>
2020-06-13 17:27:16 +0300

    finished 6th task

--- js/task-06.js
+++ js/task-06.js
@@ -1,19 +1,26 @@
-let input;
-let total = 0;
+const button6 = document.getElementById('button-6'),
+  totalSum = document.querySelector('.total-sum');
+let input,
+  total = 0;
 
+button6.addEventListener('click', event => {
+  event.preventDefault();
+  Tinycon.setBubble(6);
 
-for (let i = 1; i > 0; i += 1) {
-  input = prompt('Введите любое число');
+  for (let i = 1; i > 0; i += 1) {
+    let cancelCheck = prompt(
+      'Введите любое число (для выхода нажмите "cancel")',
+    );
+    input = Number(cancelCheck);
 
-  if (Number.isNaN(input)) {
-    alert('Было введено не число, попробуйте еще раз!');
-    continue;
-  } else if (input === null) {
-    alert(`Общая сумма чисел равна ${total}`);
-    break;
+    if (Number.isNaN(input)) {
+      alert('Было введено не число, попробуйте еще раз!');
+      continue;
+    } else if (cancelCheck === null) {
+      break;
+    }
+
+    total += input;
+    totalSum.textContent = total;
   }
-
-  total += Number(input);
-}
-
-Tinycon.setBubble(6);
+});
