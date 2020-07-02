99a99802b66dbf3a4f586774167eef0e93f591c1
massqeen <62159695+massqeen@users.noreply.github.com>
2020-07-02 20:00:30 +0300

    prepared task 2

--- js/task-02.js
+++ js/task-02.js
@@ -2,20 +2,20 @@ const button2 = document.getElementById('button-2'),
   solution2 = document.querySelector('.solution-2');
 
 button2.addEventListener('click', event => {
-    event.preventDefault();
+  event.preventDefault();
 
-    Tinycon.setBubble(2);
+  Tinycon.setBubble(2);
 
-    // const logItems = array => {
-    //   const arrayResult = [];
-    //   for (let i = 0; i < array.length; i += 1) {
-    //     let output = `${i + 1} - ${array[i]}`;
-    //     arrayResult.push(output);
-    //   }
-    //   return arrayResult.join(', ');
-    // };
-    // const result1 = logItems(arrayNames);
-    // solution1.insertAdjacentHTML('beforeend', `<p>${result1};</p>`);
-    // const result2 = logItems(arrayNumbers);
-    // solution1.insertAdjacentHTML('beforeend', `<p>${result2};</p>`);
+  // const logItems = array => {
+  //   const arrayResult = [];
+  //   for (let i = 0; i < array.length; i += 1) {
+  //     let output = `${i + 1} - ${array[i]}`;
+  //     arrayResult.push(output);
+  //   }
+  //   return arrayResult.join(', ');
+  // };
+  // const result1 = logItems(arrayNames);
+  // solution1.insertAdjacentHTML('beforeend', `<p>${result1};</p>`);
+  // const result2 = logItems(arrayNumbers);
+  // solution1.insertAdjacentHTML('beforeend', `<p>${result2};</p>`);
 });
