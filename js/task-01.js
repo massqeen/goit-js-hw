9be2d8ac05b25c91ec79ce3f331c7d27b04060fc
massqeen <62159695+massqeen@users.noreply.github.com>
2020-07-02 17:58:14 +0300

    changed the html formatting settings, started 1st js file

--- js/task-01.js
+++ js/task-01.js
@@ -1,6 +1,6 @@
 'use strict';
-let name = 'Генератор защитного поля';
-let price = 1000;
+const arrayNames = ['Mango', 'Poly', 'Ajax', 'Lux', 'Jay', 'Kong'],
+  arrayNumbers = [5, 10, 15, 20, 25, 30, 35, 40, 45, 50];
 
 console.log(`Выбран ${name}, цена за штуку ${price} кредитов`);
 price = 2000;
