5a1d04556169b0af5982f4e7668ad5c23c3bea52
massqeen <62159695+massqeen@users.noreply.github.com>
2020-07-02 18:02:08 +0300

    removed unused code

--- js/task-01.js
+++ js/task-01.js
@@ -1,7 +1,3 @@
 'use strict';
 const arrayNames = ['Mango', 'Poly', 'Ajax', 'Lux', 'Jay', 'Kong'],
   arrayNumbers = [5, 10, 15, 20, 25, 30, 35, 40, 45, 50];
-
-console.log(`Выбран ${name}, цена за штуку ${price} кредитов`);
-price = 2000;
-console.log(`Выбран ${name}, цена за штуку ${price} кредитов`);
