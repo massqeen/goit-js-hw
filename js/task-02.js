40c338025cff672d3c0d216b6d7cf7ecb3b0d64f
massqeen <62159695+massqeen@users.noreply.github.com>
2020-07-02 19:59:29 +0300

    prepared task 2

--- js/task-02.js
+++ js/task-02.js
@@ -1,7 +1,21 @@
-'use strict';
-let ordered = 50;
-let total = 100;
+const button2 = document.getElementById('button-2'),
+  solution2 = document.querySelector('.solution-2');
 
-if (ordered >= total) {
-    console.log('На складе недостаточно товаров!');
-} else console.log('Заказ оформлен, с вами свяжется менеджер');
+button2.addEventListener('click', event => {
+    event.preventDefault();
+
+    Tinycon.setBubble(2);
+
+    // const logItems = array => {
+    //   const arrayResult = [];
+    //   for (let i = 0; i < array.length; i += 1) {
+    //     let output = `${i + 1} - ${array[i]}`;
+    //     arrayResult.push(output);
+    //   }
+    //   return arrayResult.join(', ');
+    // };
+    // const result1 = logItems(arrayNames);
+    // solution1.insertAdjacentHTML('beforeend', `<p>${result1};</p>`);
+    // const result2 = logItems(arrayNumbers);
+    // solution1.insertAdjacentHTML('beforeend', `<p>${result2};</p>`);
+});
