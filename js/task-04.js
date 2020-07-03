f49f88ed29aebf04bacfb8beedcf02c1bbf291d8
massqeen <62159695+massqeen@users.noreply.github.com>
2020-07-03 18:51:50 +0300

    finished task4

--- js/task-04.js
+++ js/task-04.js
@@ -1,31 +1,17 @@
 const button4 = document.getElementById('button-4'),
-  creditsOutput = document.querySelector('.credits'),
-  droidPrice = 3000,
-  droidsInput = document.getElementById('droid'),
-  droidsOutput = document.querySelector('.droids-number');
-let credits = 23580,
-  droids = 0,
-  droidsTotal = 0;
+  solution4 = document.querySelector('.solution-4'),
+  symbols = document.getElementById('symbols');
 
-button4.addEventListener('click', event => {
+button4.addEventListener('click', (event) => {
   event.preventDefault();
-  droids = Number(droidsInput.value);
 
   Tinycon.setBubble(4);
 
-  if (droids <= 0 || Number.isNaN(droids)) {
-    alert('Пожалуйста, введите число больше 0!');
-  } else if (Number.isInteger(droids)) {
-    if (credits >= droids * droidPrice) {
-      credits -= droids * droidPrice;
-      droidsTotal += droids;
-      creditsOutput.textContent = credits;
-      droidsOutput.textContent = droidsTotal;
-      creditsOutput.style.color = credits < 3000 ? '#E52F20' : '#000';
-    } else {
-      alert('На вашем счете недостаточно кредитов :(');
+  const formatString = () => {
+    if (symbols.value.length <= 40) {
+      return symbols.value;
     }
-  } else {
-    alert('Извините, мы не продаем дроидов по частям!');
-  }
+    return symbols.value.slice(0, 39) + '.' + '.' + '.';
+  };
+  solution4.textContent = formatString();
 });
