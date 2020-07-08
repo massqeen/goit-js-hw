974bfb0a735ed36af67a80d552a8282ea9f79074
massqeen <62159695+massqeen@users.noreply.github.com>
2020-07-09 01:16:50 +0300

    prepared task 7

--- js/task-07.js
+++ js/task-07.js
@@ -1,30 +1,82 @@
-const button7 = document.getElementById('button-7'),
-  login = document.getElementById('login'),
-  logins = ['google', 'robot', 'qwerty'],
-  solution7 = document.querySelector('.solution-7');
+/* Работаем с коллекцией товаров в корзине:
+ * - getItems()
+ * - add(product)
+ * - remove(productName)
+ * - clear()
+ * - countTotalPrice()
+ * - increaseQuantity(productName)
+ * - decreaseQuantity(productName)
+ *
+ * { name: 'apple', price: 50 }
+ * { name: 'grapes', price: 70 }
+ * { name: 'lemon', price: 60 }
+ * { name: 'strawberry', price: 110 }
+ */
+const account = {
+  balance: 0,
+  transactions: [],
 
-const isLoginValid = () => {
-  return login.value.length >= 4 && login.value.length <= 16;
-};
+  getItems() {
+    return this.items;
+  },
 
-const isLoginUnique = (loginsArr) => {
-  return !loginsArr.includes(login.value);
-};
+  add(product) {
+    for (const item of this.items) {
+      if (item.name === product.name) {
+        item.quantity += 1;
+        console.log('Такой продукт уже есть, кол-во увеличено');
+        return;
+      }
+    }
+    product.quantity = 1;
+    this.items.push(product);
+    console.log('Продукт добавлен');
+  },
 
-const addLogin = () => {
-  if (!isLoginValid()) {
-    return 'Ошибка! Логин должен быть от 4 до 16 символов';
+  remove(productName) {
+    const items = this.items;
+    for (let i = 0; i < items.length; i += 1) {
+      if (items[i].name === productName) {
+        console.log('Продукт найден, удаляем!');
+        items.splice(i, 1);
+      }
+    }
+  },
+
+  clear() {
+    this.items = [];
+    console.log('Корзина очищена');
+  },
+
+  countTotalPrice() {
+    let total = 0;
+    for (const item of this.items) {
+      total += item.price * item.quantity;
+    }
+    return total;
+  },
+
+  changeQuantity(productName, quantityIncrement) {
+    for (const item of this.items) {
+      if (quantityIncrement > 0) {
+        if (productName === item.name) {
+          item.quantity += quantityIncrement;
+          console.log('Количество увеличено');
+          return;
+        }
+      }
+      if (productName === item.name) {
+        if (item.quantity === 1) {
+          this.remove(productName);
+          console.log('Продукт удален из корзины');
+          return;
+        }
+        item.quantity += quantityIncrement;
+        console.log('Количество уменьшено');
+        return;
+      }
+    }
   }
-  if (!isLoginUnique(logins)) {
-    return 'Такой логин уже используется!';
-  }
-  logins.push(login.value);
-  return 'Логин успешно добавлен!';
 };
 
-button7.addEventListener('click', (event) => {
-  event.preventDefault();
-  Tinycon.setBubble(7);
-
-  solution7.textContent = addLogin();
-});
+export default cart;
