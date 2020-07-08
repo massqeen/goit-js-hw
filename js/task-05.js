3ea7ff769a0a3eb739b4d1c6d4abbd2f51e20efb
massqeen <62159695+massqeen@users.noreply.github.com>
2020-07-09 00:48:08 +0300

    added task 5, removed double-negation checking in eslint

--- js/task-05.js
+++ js/task-05.js
@@ -1,23 +1,32 @@
 const button5 = document.getElementById('button-5'),
-  solution5 = document.querySelector('.solution-5'),
-  spam = document.getElementById('spam');
+  products = [
+    { name: 'Радар', price: 1300, quantity: 4 },
+    { name: 'Сканер', price: 2700, quantity: 3 },
+    { name: 'Дроид', price: 400, quantity: 7 },
+    { name: 'Захват', price: 1200, quantity: 2 }
+  ],
+  property = document.getElementById('property'),
+  solution5 = document.querySelector('.solution-5');
 
-const checkForSpam = () => {
-  const array = spam.value.toLowerCase().split(' ');
-  const wordsToCheck = ['spam', 'sale', 'спам', 'распродажа'];
-  for (const word of wordsToCheck) {
-    if (array.includes(word)) {
-      return true;
+const getAllPropValues = (arr, prop) => {
+  const allValues = [];
+  for (const objItem of arr) {
+    if (objItem[prop]) {
+      allValues.push(objItem[prop]);
     }
   }
-  return false;
+  return allValues;
 };
 
 button5.addEventListener('click', (event) => {
   event.preventDefault();
   Tinycon.setBubble(5);
+  const result = getAllPropValues(products, property.value);
 
-  solution5.textContent = checkForSpam()
-    ? 'Обнаружен спам!'
-    : 'Ура, спама нет!';
+  if (!!result.length) {
+    solution5.textContent = result.join(', ');
+  } else {
+    solution5.textContent =
+      'В исходном массиве нет объектов с таким свойством!';
+  }
 });
