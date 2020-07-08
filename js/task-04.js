871e5ef0893b03ddaf060e72761ba70e30f4c0a8
massqeen <62159695+massqeen@users.noreply.github.com>
2020-07-08 23:50:19 +0300

    added task 4

--- js/task-04.js
+++ js/task-04.js
@@ -1,17 +1,33 @@
 const button4 = document.getElementById('button-4'),
-  solution4 = document.querySelector('.solution-4'),
-  symbols = document.getElementById('symbols');
+  buttonSolve = document.getElementById('button-4-1'),
+  employees = {},
+  nameEmployee = document.getElementById('nameEmployeeTask4'),
+  salaryEmployee = document.getElementById('salaryEmployee'),
+  solution4 = document.querySelector('.solution-4');
 
-const formatString = () => {
-  if (symbols.value.length <= 40) {
-    return symbols.value;
+const countTotalSalary = (obj) => {
+  let totalSalary = 0;
+
+  for (const value of Object.values(obj)) {
+    totalSalary += Number(value);
   }
-  return symbols.value.slice(0, 39) + '...';
-};
 
+  return totalSalary;
+};
 button4.addEventListener('click', (event) => {
   event.preventDefault();
   Tinycon.setBubble(4);
 
-  solution4.textContent = formatString();
+  employees[nameEmployee.value] = salaryEmployee.value;
+  console.log(employees);
+});
+
+buttonSolve.addEventListener('click', () => {
+  const totalSalary = countTotalSalary(employees);
+
+  solution4.insertAdjacentHTML(
+    'beforeend',
+    `<p>Сумма зарплаты работников составляет: <span class="task4-span">${totalSalary}</span> кредитов.</p>`
+  );
+  document.querySelector('.task4-span').style.color = '#2ac940';
 });
