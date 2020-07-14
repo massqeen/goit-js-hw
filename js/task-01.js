d44e8d73eeb5f835b8e585aca5e298bc411f4f07
massqeen <62159695+massqeen@users.noreply.github.com>
2020-07-15 00:46:17 +0300

    finished task 1

--- js/task-01.js
+++ js/task-01.js
@@ -1,7 +1,14 @@
+const addOrderBtn = document.getElementById('button-1-addOrder'),
+  addOrderInput = document.getElementById('orderPrice'),
+  discountBtn = document.getElementById('button-1-discount'),
+  discountInput = document.getElementById('discount'),
+  ordersBtn = document.getElementById('button-1-orders'),
+  solution = document.querySelector('.solution-1');
+
 const account = {
   owner: 'Mango',
   balance: 24000,
-  discount: 0.1,
+  discount: 0,
   orders: ['order-1', 'order-2', 'order-3'],
   changeDiscount(value) {
     this.discount = value;
@@ -9,17 +16,48 @@ const account = {
   showOrders() {
     return this.orders;
   },
-  addOrder(cost, order) {
-    this.balance -= cost;
-    this.orders.push(order);
+  addOrder(cost) {
+    this.balance -= cost * (1 - this.discount);
+    this.orders.push(`order-${this.orders.length + 1}`);
   }
 };
 
-account.changeDiscount(0.15);
-console.log(account.discount); // 0.15
+const handleDiscountBtnClick = (event) => {
+  event.preventDefault();
+  Tinycon.setBubble(7);
+  if (discountInput.value >= 0 && discountInput.value <= 0.5) {
+    account.changeDiscount(discountInput.value);
+    solution.textContent = `Скидка установлена в размере ${
+      account.discount * 100
+    }%`;
+    return;
+  }
+  alert('Введите число от 0 до 0,5!');
+};
 
-console.table(account.showOrders()); // ['order-1', 'order-2', 'order-3']
+const handleOrdersBtnClick = (event) => {
+  event.preventDefault();
+  Tinycon.setBubble(7);
+  solution.textContent = `Подтвержденные заказы: ${account
+    .showOrders()
+    .join(', ')}.`;
+};
 
-account.addOrder(5000, 'order-4');
-console.log(account.balance); // 19000
-console.table(account.showOrders()); // ['order-1', 'order-2', 'order-3', 'order-4']
+const handleAddOrderBtnClick = (event) => {
+  event.preventDefault();
+  Tinycon.setBubble(7);
+  if (addOrderInput.value > 0 && addOrderInput.value <= account.balance) {
+    account.addOrder(addOrderInput.value);
+    solution.textContent = `Заказ подтвержден! Ваш баланс: ${account.balance}`;
+    return;
+  }
+  if (addOrderInput.value > account.balance) {
+    alert('Недостаточно средств на счету :(');
+    return;
+  }
+  alert('Введите число больше  0!');
+};
+
+discountBtn.addEventListener('click', handleDiscountBtnClick);
+ordersBtn.addEventListener('click', handleOrdersBtnClick);
+addOrderBtn.addEventListener('click', handleAddOrderBtnClick);
--- js/task-02.js
+++ js/task-02.js
@@ -1,11 +1,30 @@
-const button2 = document.getElementById('button-2'),
-  object = { mail: 'poly@mail.com', isOnline: true, score: 500 },
-  solution2 = document.querySelector('.solution-2');
+const inventory = {
+  items: ['Knife', 'Gas mask'],
+  add(itemName) {
+    console.log(`Adding ${itemName} to inventory`);
 
-const countProps = (obj) => Object.values(obj).length;
-button2.addEventListener('click', (event) => {
-  event.preventDefault();
-  Tinycon.setBubble(2);
+    this.items.push(itemName);
+  },
+  remove(itemName) {
+    console.log(`Removing ${itemName} from inventory`);
 
-  solution2.textContent = `В объекте ${countProps(object)} свойств`;
-});
+    this.items = this.items.filter((item) => item !== itemName);
+  }
+};
+
+const invokeInventoryAction = function (itemName, action) {
+  console.log(`Invoking action on ${itemName}`);
+  action.bind(inventory)(itemName);
+};
+
+invokeInventoryAction('Medkit', inventory.add);
+// Invoking action on Medkit
+// Adding Medkit to inventory
+
+console.log(inventory.items); // ['Knife', 'Gas mask', 'Medkit']
+
+invokeInventoryAction('Gas mask', inventory.remove);
+// Invoking action on Gas mask
+// Removing Gas mask from inventory
+
+console.log(inventory.items); // ['Knife', 'Medkit']
