b59f0adf4f82e5038d0721e4259e2f13c201ef17
massqeen <62159695+massqeen@users.noreply.github.com>
2020-07-14 19:17:50 +0300

    updated returns to template strings in task 3 and 4, replaced 'if' in task 4 with ternary operator.

--- js/task-03.js
+++ js/task-03.js
@@ -1,18 +1,16 @@
-const answerDescription = document.querySelector('.answer-description'),
-  answerWords = document.querySelector('.answer-words'),
-  answerWordsLength = document.querySelector('.answer-words-length'),
-  button3 = document.getElementById('button-3'),
+const button3 = document.getElementById('button-3'),
+  solution3 = document.querySelector('.solution-3'),
   string = document.getElementById('string-3');
 
 const findLongestWord = () => {
   const words = string.value.split(' ');
   let longestWord = '';
-  const longestWords = [];
+  let longestWords = [];
 
   for (const word of words) {
     if (word.length > longestWord.length) {
       longestWord = word;
-      longestWords.splice(0, 100, longestWord);
+      longestWords = [longestWord];
     }
     if (word.length === longestWord.length && word !== longestWord) {
       longestWords.push(word);
@@ -20,26 +18,16 @@ const findLongestWord = () => {
   }
 
   if (longestWords.length > 1) {
-    return [
-      'Самые длинные слова в вашей фразе: ',
-      longestWords.join(', ') + '; ',
-      `их длина равна ${longestWord.length} символам.`
-    ];
+    return `Самые длинные слова в вашей фразе: ${longestWords.join(
+      ', '
+    )}; их длина равна ${longestWord.length} символам.`;
   }
-  return [
-    'Самое длинное слово в вашей фразе: ',
-    longestWord + '; ',
-    `его длина равна ${longestWord.length} символам.`
-  ];
+  return `Самое длинное слово в вашей фразе: ${longestWord}; его длина равна ${longestWord.length} символам.`;
 };
 
 button3.addEventListener('click', (event) => {
   event.preventDefault();
   Tinycon.setBubble(3);
 
-  const resultArray = findLongestWord();
-  answerWords.style.color = '#2ac940';
-  answerDescription.textContent = resultArray[0];
-  answerWords.textContent = resultArray[1];
-  answerWordsLength.textContent = resultArray[2];
+  solution3.textContent = findLongestWord();
 });
