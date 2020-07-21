dcb8c1b3e5ab2202ac39777ccc660ab698a29a69
massqeen <62159695+massqeen@users.noreply.github.com>
2020-07-21 23:08:42 +0300

    finished task 1

--- js/task-01.js
+++ js/task-01.js
@@ -3,24 +3,20 @@ const button1 = document.getElementById('button-1'),
   login = document.getElementById('login'),
   solution1 = document.querySelector('.solution-1');
 
+const Account = function (inputLogin, inputEmail) {
+  this.login = inputLogin;
+  this.email = inputEmail;
+};
+Account.prototype.getInfo = function () {
+  solution1.textContent = `Login: ${this.login}, Email: ${this.email}`;
+};
+
 const handleButtonClick = (event) => {
   event.preventDefault();
   Tinycon.setBubble(1);
-
-  solution1.textContent = '';
-  user.name = userName.value;
-  user.mood = 'happy';
-  user.hobby = 'skydiving';
-  user.premium = false;
-  for (const key of Object.keys(user)) {
-    solution1.insertAdjacentHTML(
-      'beforeend',
-      `<p>${key}: <span class="task1-span">${user[key]}</span></p>`
-    );
-    document
-      .querySelectorAll('.task1-span')
-      .forEach((element) => (element.style.color = '#2ac940'));
-  }
+  const account = new Account(login.value, email.value);
+  account.getInfo();
+  console.log(account);
 };
 
 button1.addEventListener('click', handleButtonClick);
