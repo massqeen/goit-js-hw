a172dd62d72b6967b36b83ff466e4fe6d722dd62
massqeen <62159695+massqeen@users.noreply.github.com>
2020-07-21 23:32:54 +0300

    finished task 2

--- js/task-02.js
+++ js/task-02.js
@@ -1,12 +1,26 @@
 const button2 = document.getElementById('button-2'),
-  object = { mail: 'poly@mail.com', isOnline: true, score: 500 },
-  solution2 = document.querySelector('.solution-2');
+  solution2 = document.querySelector('.solution-2'),
+  userAge = document.getElementById('userAge'),
+  userFollowers = document.getElementById('userFollowers'),
+  userName = document.getElementById('userName');
 
-const countProps = (obj) => Object.values(obj).length;
+class User {
+  constructor(name, age, followers) {
+    this.name = name;
+    this.age = age;
+    this.followers = followers;
+  }
+  getInfo() {
+    return `User ${this.name} is ${this.age} years old and has ${this.followers} followers`;
+  }
+}
+
 const handleButtonClick = (event) => {
   event.preventDefault();
   Tinycon.setBubble(2);
-  solution2.textContent = `В объекте ${countProps(object)} свойств`;
+  const user = new User(userName.value, userAge.value, userFollowers.value);
+  console.log(user);
+  solution2.textContent = user.getInfo();
 };
 
 button2.addEventListener('click', handleButtonClick);
