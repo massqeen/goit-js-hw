4148035bb186c7b218e42ca7eaa718f4d394805c
Arthur Plyuschevsky <62159695+massqeen@users.noreply.github.com>
2020-07-27 10:52:44 +0300

    merge conflicts

--- js/task-02.js
+++ js/task-02.js
@@ -1,7 +1,3 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> gh-pages
 const button2 = document.getElementById('button-2'),
   solution2 = document.querySelector('.solution-2'),
   userAge = document.getElementById('userAge'),
@@ -20,60 +16,11 @@ class User {
 }
 
 const handleButtonClick = (event) => {
-<<<<<<< HEAD
   event.preventDefault();
   Tinycon.setBubble(2);
   const user = new User(userName.value, userAge.value, userFollowers.value);
   console.log(user);
   solution2.textContent = user.getInfo();
-=======
-const addItemBtn = document.getElementById('button-2-addItem'),
-  addItemInput = document.getElementById('addItem'),
-  removeItemBtn = document.getElementById('button-2-removeItem'),
-  removeItemInput = document.getElementById('removeItem'),
-  solution = document.querySelector('.solution-2');
-
-const inventory = {
-  items: ['Knife', 'Gas mask'],
-  add(itemName) {
-    console.log(`Adding ${itemName} to inventory`);
-    this.items.push(itemName);
-  },
-  remove(itemName) {
-    console.log(`Removing ${itemName} from inventory`);
-    this.items = this.items.filter((item) => item !== itemName);
-  }
 };
 
-const invokeInventoryAction = function (itemName, action) {
-  console.log(`Invoking action on ${itemName}`);
-  action(itemName);
-};
-
-const handleAddItemBtnClick = (event) => {
-=======
->>>>>>> gh-pages
-  event.preventDefault();
-  Tinycon.setBubble(2);
-
-<<<<<<< HEAD
-  invokeInventoryAction(addItemInput.value, inventory.add.bind(inventory));
-  solution.textContent = `В инвентаре: ${inventory.items.join(', ')}.`;
-};
-
-const handleRemoveItemBtnClick = (event) => {
-  event.preventDefault();
-  Tinycon.setBubble(2);
-
-  invokeInventoryAction(
-    removeItemInput.value,
-    inventory.remove.bind(inventory)
-  );
-  solution.textContent = `В инвентаре: ${inventory.items.join(', ')}.`;
->>>>>>> 1652ee1d2a434292091dcaa4295b6aac04105084
-};
-
-=======
->>>>>>> gh-pages
-addItemBtn.addEventListener('click', handleAddItemBtnClick);
-removeItemBtn.addEventListener('click', handleRemoveItemBtnClick);
+button2.addEventListener('click', handleButtonClick);
