b664c02f7057a3d374f77199c0231b0fb12a7413
massqeen <62159695+massqeen@users.noreply.github.com>
2020-08-15 16:54:55 +0300

    updated task 3 (styles of grid), added task 4

--- js/task-03.js
+++ js/task-03.js
@@ -2,6 +2,21 @@ const galleryRef = document.getElementById('gallery');
 const images = [
   {
     url:
+      'https://images.unsplash.com/photo-1593642532842-98d0fd5ebc1a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80',
+    alt: '"Notebook"'
+  },
+  {
+    url:
+      'https://images.unsplash.com/photo-1568378259895-f15089a7f544?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1345&q=80',
+    alt: '"Wall"'
+  },
+  {
+    url:
+      'https://images.unsplash.com/photo-1597226417297-6b99b273ded6?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80',
+    alt: '"Desert"'
+  },
+  {
+    url:
       'https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
     alt: '"White and Black Long Fur Cat"'
   },
--- js/task-04.js
+++ js/task-04.js
@@ -1,32 +1,17 @@
-const button4 = document.getElementById('button-4'),
-  buttonSolve = document.getElementById('button-4-1'),
-  employees = {},
-  nameEmployee = document.getElementById('nameEmployeeTask4'),
-  salaryEmployee = document.getElementById('salaryEmployee'),
-  solution4 = document.querySelector('.solution-4');
+const decrementRef = document.querySelector('button[data-action="decrement"]'),
+  incrementRef = document.querySelector('button[data-action="increment"]'),
+  valueRef = document.getElementById('value');
 
-const countTotalSalary = (obj) => {
-  return Object.values(obj).reduce(
-    (totalSalary, currentSalary) => totalSalary + +currentSalary,
-    0
-  );
+let counterValue = 0;
+const decrement = () => {
+  counterValue -= 1;
+  showCounterValue();
 };
-const handleButton4Click = (event) => {
-  event.preventDefault();
-  Tinycon.setBubble(4);
-
-  employees[nameEmployee.value] = salaryEmployee.value;
-  console.log(employees);
+const increment = () => {
+  counterValue += 1;
+  showCounterValue();
 };
-const handleButtonSolveClick = () => {
-  const totalSalary = countTotalSalary(employees);
-  solution4.textContent = '';
-  solution4.insertAdjacentHTML(
-    'beforeend',
-    `<p>Сумма зарплаты работников составляет: <span class="task4-span">${totalSalary}</span> кредитов.</p>`
-  );
-  document.querySelector('.task4-span').style.color = '#2ac940';
-};
-
-button4.addEventListener('click', handleButton4Click);
-buttonSolve.addEventListener('click', handleButtonSolveClick);
+const showCounterValue = () => (valueRef.textContent = counterValue);
+decrementRef.addEventListener('click', decrement);
+incrementRef.addEventListener('click', increment);
+valueRef.textContent = counterValue;
