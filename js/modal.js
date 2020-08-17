19e80c7558d40b489b45e4a347cf5269c47efc18
Arthur Plyuschevsky <62159695+massqeen@users.noreply.github.com>
2020-08-17 17:58:26 +0300

    added modal workshop

---
+++ js/modal.js
@@ -0,0 +1,34 @@
+const refs = {
+  openModal: document.querySelector('.button[data-action="open-modal"]'),
+  backdrop: document.querySelector('.js-backdrop')
+};
+
+const openModalHandler = () => {
+  document.body.classList.add('show-modal');
+  window.addEventListener('keydown', modalEscapeHandler);
+};
+
+const closeModalHandler = () => {
+  document.body.classList.remove('show-modal');
+  window.removeEventListener('keydown', modalEscapeHandler);
+};
+
+const modalEscapeHandler = (e) => {
+  console.log(e.code);
+  if (e.code === 'Escape') {
+    closeModalHandler();
+  }
+};
+
+const backdropClickHandler = (e) => {
+  if (
+    e.target === e.currentTarget ||
+    e.target ===
+      refs.backdrop.querySelector('.button[data-action="close-modal"]')
+  ) {
+    closeModalHandler();
+  }
+};
+
+refs.openModal.addEventListener('click', openModalHandler);
+refs.backdrop.addEventListener('click', (e) => backdropClickHandler(e));
