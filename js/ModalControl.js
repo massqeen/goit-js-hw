123d929e364e192117369be2c24250a6ce0ac461
massqeen <62159695+massqeen@users.noreply.github.com>
2020-08-19 00:52:03 +0300

    added slider for img navigation in modal

--- js/ModalControl.js
+++ js/ModalControl.js
@@ -1,6 +1,8 @@
 export default class ModalControl {
   constructor(modalRef, galleryItems) {
     this._modalRef = modalRef;
+    this._buttonPrev = this._modalRef.querySelector('.js-lightbox-prev');
+    this._buttonNext = this._modalRef.querySelector('.js-lightbox-next');
     this._modalImageRef = this._modalRef.querySelector('.lightbox__image');
     this._galleryItems = galleryItems;
     this._imgId = 0;
@@ -11,14 +13,22 @@ export default class ModalControl {
   }
 
   set imgId(value) {
-    this._imgId = +value;
+    this._imgId = value;
   }
 
   openModal() {
     this.resetImgSrc();
     this.bodyLock();
+    if (this.imgId > 0) {
+      this.setButtonPrevActive();
+    }
+    if (this.imgId < this._galleryItems.length - 1) {
+      this.setButtonNextActive();
+    }
     this._modalRef.classList.add('is-open');
     window.addEventListener('keydown', this.keyPressHandler);
+    this._buttonPrev.addEventListener('click', this.sliderPrevClickHandler);
+    this._buttonNext.addEventListener('click', this.sliderNextClickHandler);
   }
 
   keyPressHandler = (e) => {
@@ -33,22 +43,45 @@ export default class ModalControl {
     }
   };
 
+  sliderPrevClickHandler = () => {
+    this.prevImage();
+  };
+
+  sliderNextClickHandler = () => {
+    this.nextImage();
+  };
+
   prevImage() {
     this._modalImageRef.src = this._galleryItems[+this.imgId - 1].original;
     this.imgId -= 1;
-    console.log(this.imgId);
+    if (this.imgId === 0) {
+      this.setButtonPrevInactive();
+    }
+    if (this.imgId < this._galleryItems.length - 1) {
+      this.setButtonNextActive();
+    }
   }
 
   nextImage() {
     this._modalImageRef.src = this._galleryItems[+this.imgId + 1].original;
     this.imgId += 1;
-    console.log(this.imgId);
+    if (this.imgId === this._galleryItems.length - 1) {
+      this.setButtonNextInactive();
+    }
+    if (this.imgId > 0) {
+      this.setButtonPrevActive();
+    }
   }
 
   closeModal = () => {
     window.removeEventListener('keydown', this.keyPressHandler);
+    this._buttonPrev.removeEventListener('click', this.sliderPrevClickHandler);
+    this._buttonNext.removeEventListener('click', this.sliderNextClickHandler);
+    this.imgId = 0;
     this._modalRef.classList.remove('is-open');
     this.bodyUnLock();
+    this.setButtonPrevInactive();
+    this.setButtonNextInactive();
   };
 
   resetImgSrc() {
@@ -66,5 +99,25 @@ export default class ModalControl {
   bodyUnLock() {
     document.body.style.paddingRight = '0px';
     document.body.classList.remove('is-lock');
+  }
+
+  setButtonPrevActive() {
+    this._buttonPrev.removeAttribute('disabled');
+    this._buttonPrev.classList.remove('lightbox-slider__btn--inactive');
+  }
+
+  setButtonNextActive() {
+    this._buttonNext.removeAttribute('disabled');
+    this._buttonNext.classList.remove('lightbox-slider__btn--inactive');
+  }
+
+  setButtonPrevInactive() {
+    this._buttonPrev.setAttribute('disabled', 1);
+    this._buttonPrev.classList.add('lightbox-slider__btn--inactive');
+  }
+
+  setButtonNextInactive() {
+    this._buttonNext.setAttribute('disabled', 1);
+    this._buttonNext.classList.add('lightbox-slider__btn--inactive');
   }
 }
--- js/main.js
+++ js/main.js
@@ -3,18 +3,21 @@ import galleryItems from './gallery-items.js';
 import GalleryGenerator from './GalleryGenerator.js';
 
 const refs = {
-  galleryRef: document.querySelector('.js-gallery'),
-  lightbox: document.querySelector('.js-lightbox')
+  gallery: document.querySelector('.js-gallery'),
+  lightbox: document.querySelector('.js-lightbox'),
+  lightboxCloseButton: document.querySelector(
+    'button[data-action="close-lightbox"]'
+  )
 };
 
 const modalControl = new ModalControl(refs.lightbox, galleryItems);
 const galleryGenerator = new GalleryGenerator(galleryItems);
 
-refs.galleryRef.append(...galleryGenerator.createAllElements());
+refs.gallery.append(...galleryGenerator.createAllElements());
 
 const galleryClickHandler = (e) => {
   if (e.target.nodeName === 'IMG') {
-    modalControl.imgId = e.target.dataset.id;
+    modalControl.imgId = +e.target.dataset.id;
     modalControl.openModal();
     const imgRef = refs.lightbox.querySelector('.lightbox__image');
     imgRef.src = e.target.dataset.src;
@@ -23,10 +26,10 @@ const galleryClickHandler = (e) => {
 };
 
 const lightboxClickHandler = (e) => {
-  if (e.target.nodeName === 'BUTTON') {
+  if (e.target === refs.lightboxCloseButton) {
     modalControl.closeModal();
   }
 };
 
-refs.galleryRef.addEventListener('click', galleryClickHandler);
+refs.gallery.addEventListener('click', galleryClickHandler);
 refs.lightbox.addEventListener('click', lightboxClickHandler);
