d1d7f13c40486ac51f4c9f9eb5581e0920c00e9c
massqeen <62159695+massqeen@users.noreply.github.com>
2020-08-18 20:04:01 +0300

    added method to close modal on Esc press

--- js/ModalControl.js
+++ js/ModalControl.js
@@ -4,8 +4,14 @@ export default class ModalControl {
   }
   openModal() {
     this._modalRef.classList.add('is-open');
+    window.addEventListener('keydown', this.escPressHandler);
   }
-
+  escPressHandler = (e) => {
+    if (e.code === 'Escape') {
+      window.removeEventListener('keydown', this.escPressHandler);
+      this.closeModal();
+    }
+  };
   closeModal() {
     this._modalRef.classList.remove('is-open');
   }
