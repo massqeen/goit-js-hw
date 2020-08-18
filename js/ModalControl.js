d6282008df3135e5cf95b6675133bb97387f2e5e
massqeen <62159695+massqeen@users.noreply.github.com>
2020-08-18 21:47:05 +0300

    added scroll-block/unblock on modal opening/closing

--- js/ModalControl.js
+++ js/ModalControl.js
@@ -5,6 +5,7 @@ export default class ModalControl {
 
   openModal() {
     this.resetImgSrc();
+    this.bodyLock();
     this._modalRef.classList.add('is-open');
     window.addEventListener('keydown', this.escPressHandler);
   }
@@ -18,9 +19,23 @@ export default class ModalControl {
 
   closeModal = () => {
     this._modalRef.classList.remove('is-open');
+    this.bodyUnLock();
   };
 
   resetImgSrc() {
     this._modalRef.querySelector('.lightbox__image').src = '';
+  }
+
+  bodyLock() {
+    document.body.style.paddingRight =
+      window.innerWidth -
+      document.querySelector('.container').offsetWidth +
+      'px';
+    document.body.classList.add('is-lock');
+  }
+
+  bodyUnLock() {
+    document.body.style.paddingRight = '0px';
+    document.body.classList.remove('is-lock');
   }
 }
