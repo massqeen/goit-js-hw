554f00b842532e3891732a330c88129ad6156c97
Arthur Plyuschevsky <62159695+massqeen@users.noreply.github.com>
2020-08-23 11:56:12 +0300

    updated task 5 fixed empty name

--- js/task-05.js
+++ js/task-05.js
@@ -1,6 +1,6 @@
 const inputRef = document.getElementById('name-input'),
   outputRef = document.getElementById('name-output');
 const inputHandler = () => {
-  outputRef.textContent = inputRef.value;
+  outputRef.textContent = inputRef.value ? inputRef.value : 'незнакомец';
 };
 inputRef.addEventListener('input', inputHandler);
