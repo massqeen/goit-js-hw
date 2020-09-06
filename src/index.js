4c9be0c29ca3af1c7553f6bc68efc64fad3ec31c
massqeen <62159695+massqeen@users.noreply.github.com>
2020-09-07 01:12:49 +0300

    trying to fix pnotify animation

--- src/index.js
+++ src/index.js
@@ -1,11 +1,10 @@
 import './scss/main.scss';
 const debounce = require('lodash/debounce');
 import '@pnotify/core/dist/PNotify.css';
-
 import '@pnotify/core/dist/BrightTheme.css';
 import updateCountriesMarkup from './js/updateCountriesMarkup';
+import PNotify from '@pnotify/core/dist/PNotify.js';
 import { defaultModules } from '@pnotify/core/dist/PNotify.js';
-
 import * as PNotifyAnimate from '@pnotify/animate';
 import fetchCountry from './js/fetch';
 import refs from './js/refs';
@@ -25,17 +24,21 @@ const inputHandler = ({ target: { value } }) => {
         searchErrorNotFound(err);
       });
 };
+
+import { notice, error } from '@pnotify/core';
+const test = [
+  ...defaultModules,
+  [
+    PNotifyAnimate,
+    {
+      inClass: 'rotateInUpRight',
+      outClass: 'rotateOutUpRight'
+    }
+  ]
+];
+console.log(test);
 refs.search.addEventListener('input', debounce(inputHandler, 800));
-// notice({
-//   text: 'I use the Animate module in addition to the defaults.',
-//   modules: new Map([
-//     ...defaultModules,
-//     [
-//       PNotifyAnimate,
-//       {
-//         inClass: 'fadeInDown',
-//         outClass: 'fadeOutUp'
-//       }
-//     ]
-//   ])
-// });
+notice({
+  text: 'I use the Animate module in addition to the defaults.',
+  modules: new Map([test])
+});
