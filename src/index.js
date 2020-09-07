c6a31ba17e3191476a27c16cab14ad1de6dd2912
Arthur Plyuschevsky <62159695+massqeen@users.noreply.github.com>
2020-09-07 15:17:54 +0300

    added fetch spinner

--- src/index.js
+++ src/index.js
@@ -3,37 +3,44 @@ const debounce = require('lodash/debounce');
 import '@pnotify/core/dist/PNotify.css';
 import '@pnotify/core/dist/BrightTheme.css';
 import updateCountriesMarkup from './js/updateCountriesMarkup';
-
+import Loader from './js/components/Loader';
 import fetchCountry from './js/fetch';
 import refs from './js/refs';
 import { searchError, searchErrorNotFound } from './js/notifyErrors';
+
+const loader = new Loader('.js-loader', 'is-hidden');
 
 const inputHandler = ({ target: { value } }) => {
   if (!value) {
     return;
   }
 
+  loader.show();
   fetchCountry(value)
     .then((countries) => {
       if (countries.length <= 10) {
-        console.log(countries);
         updateCountriesMarkup.show(countries);
+        loader.hide();
         return;
       }
+      loader.hide();
       searchError();
     })
     .catch((err) => {
+      loader.hide();
       searchErrorNotFound(err);
     });
 };
 
 const listClickHandler = (e) => {
   if (e.target.nodeName === 'A') {
-    fetchCountry(e.target.innerText).then((country) =>
-      updateCountriesMarkup.show(country)
-    );
+    loader.show();
+    fetchCountry(e.target.innerText).then((country) => {
+      loader.hide();
+      return updateCountriesMarkup.show(country);
+    });
   }
 };
 
-refs.search.addEventListener('input', debounce(inputHandler, 800));
+refs.search.addEventListener('input', debounce(inputHandler, 600));
 refs.countriesList.addEventListener('click', listClickHandler);
--- src/js/refs.js
+++ src/js/refs.js
@@ -1,6 +1,7 @@
 const refs = {
   countriesList: document.querySelector('.countries'),
   search: document.querySelector('.search'),
-  card: document.querySelector('.card')
+  card: document.querySelector('.card'),
+  loader: document.querySelector('.js-loader')
 };
 export default refs;
