29341c493403e3419c55ece58ffa271ef0471543
massqeen <62159695+massqeen@users.noreply.github.com>
2020-09-07 21:54:54 +0300

    added way of getting geolocation

--- README.md
+++ README.md
@@ -1,7 +1,3 @@
 # goit-js-hw
 
-#JS homework-10
-
-##JSON, Web Storage API, templating (Handlebars lib)
-
-[Production link](http://fls.guru/www/massqeen/hw10/)
+#JS Geolocation API
--- src/index.js
+++ src/index.js
@@ -1,16 +1,35 @@
 import './scss/main.scss';
-import './js/markupIntegration';
-import Theme from './js/Theme';
-import './js/lazyLoad';
 
-const theme = new Theme(
-  '.js-switch-input',
-  'theme',
-  'light-theme',
-  'dark-theme'
-);
-if (theme.getThemeFromLocalStorage('theme') === 'dark-theme') {
-  theme.changeTheme('light-theme', 'dark-theme');
-  theme.$el.checked = true;
-}
-theme.init();
+const onGetPositionSuccess = (location) => {
+  console.log(location);
+  console.log(location.coords);
+};
+
+const onGetPositionError = (error) => {
+  console.log(error);
+  console.log(error.code);
+  console.log(error.message);
+};
+
+// needed for mobile geolocation mostly
+// maximumAge - время кэширования координат(как часто запрашивать новую позицию), в мс
+// timeout - количество мс, которое API геолокации будет пытаться получить координаты
+// enableHighAccuracy - повышенная точность (true/false)
+
+// navigator.geolocation.getCurrentPosition(
+//   onGetPositionSuccess,
+//   onGetPositionError,
+//   options
+// );
+const getCurrentPosition = () => {
+  const options = {
+    maximumAge: 30000,
+    timeout: 5000,
+    enableHighAccuracy: false
+  };
+
+  return new Promise((resolve, reject) => {
+    navigator.geolocation.getCurrentPosition(resolve, reject, options);
+  });
+};
+getCurrentPosition().then(onGetPositionSuccess).catch(onGetPositionError);
