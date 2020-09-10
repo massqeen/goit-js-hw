7fab6c2088daa2fd6769076769f654a1d0b0547b
massqeen <62159695+massqeen@users.noreply.github.com>
2020-09-11 01:34:02 +0300

    added a template for the image card, card styles

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import './scss/main.scss';
-import imagesTemplate from './templates/images.hbs';
+import imageCardTemplate from './templates/imageCard.hbs';
 // import './js/lazyLoad';
 
 const API_KEY = '18257903-4453e2975e3dd917fd04b41f9';
@@ -12,7 +12,6 @@ fetch(`${url}&editors_choice&per_page=10`)
   .then((res) => res.json())
   .then(({ hits }) => {
     console.log(hits);
-    const markup = imagesTemplate(hits);
-    console.log(markup);
+    const markup = imageCardTemplate(hits);
     refs.gallery.innerHTML = markup;
   });
